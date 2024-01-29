0f6ae60832c7b33436c455356e7641f1e8cd2c3b
FranJRG <framirezgarcia0@gmail.com>
2024-01-29 09:27:49 +0100

    Arreglos

--- src/players/details.jsx
+++ src/players/details.jsx
@@ -1,12 +1,39 @@
-import { Link } from "react-router-dom"
+import { useEffect, useState } from "react";
+import { Link, useParams } from "react-router-dom"
+import { getPlayer } from "../services/playerService";
 
 export default function Details(){
+
+    const {_id} = useParams()
+
+    let [player,setPlayer] = useState({
+        name: "",
+        age:0,
+        team:"",
+        status:""
+    });
+
+    useEffect(()=>{
+        async function getPlayerId(){
+            getPlayer(_id).then((response)=>{
+                setPlayer(response.data)
+            });
+        }
+
+        if(_id!=null){
+            getPlayerId();
+        }
+
+    }, [_id])
+
     return(
         <>
             <div className="card" style="width: 18rem;">
                 <div className="card-body">
-                    <h5 className="card-title">Special title treatment</h5>
-                    <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                    <h5 className="card-title">{player.name}</h5>
+                    <p className="card-text">{player.team}</p>
+                    <p className="card-text">{player.age}</p>
+                    <p className="card-text">{player.status}</p>
                     <Link to="/listPlayer">Return</Link>
                 </div>
             </div>
